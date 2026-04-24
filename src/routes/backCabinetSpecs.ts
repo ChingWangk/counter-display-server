8c98518a7e33bb2731c391e555c546071777c73f
wangxiaoyong <21307110081@m.fudan.edu.cn>
2026-04-24 16:43:56 +0800

    fix back

--- src/routes/backCabinetSpecs.ts
+++ src/routes/backCabinetSpecs.ts
@@ -2,12 +2,27 @@ import { Router, Request, Response } from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
 
+interface ThemeGroup {
+  id: string;
+  label: string;
+  specIds: string[];
+  images: string[];
+}
+
 const router = Router();
 
+const IMAGE_PREFIX = '/images/back-themes/';
+
 router.get('/', (req: Request, res: Response) => {
-  const filePath = path.join(__dirname, '../data/back-cabinet-specs.json');
-  const ids: string[] = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-  res.json({ success: true, specs: ids });
+  const filePath = path.join(__dirname, '../data/back-cabinet-themes.json');
+  const themes: ThemeGroup[] = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+  const result = themes.map(t => ({
+    ...t,
+    images: t.images.map(img => IMAGE_PREFIX + img),
+  }));
+
+  res.json({ success: true, themes: result });
 });
 
 export default router;
